6c134052f8775c7bc86e6ab64e3ef3b3355b0773
Jerrwyz <jerroldwongyz@gmail.com>
2024-04-08 17:51:48 +1000

    dialogue commit

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,32 @@
 export const scaleFactor = 4;
+
+export const dialogueData = { 
+  pc: `This is my PC. I work mostly on frontend development and I would be open to backend development too.
+    I have experience using most of the popular languages such as Python, Java, C, C#, JavaScript, TypeScript, and Kotlin. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Jerroldwyz" target="_blank">Github</a>!`,
+
+  sofaTable: `That's my sofa. I like to relax here and watch YouTube.`,
+
+  tv: `That's my TV. I enjoy watching Youtube channels that offers informative insights such as:
+   <a href="https://www.youtube.com/@TEDEd" target="_blank">TED-Ed</a>, <a href="https://www.youtube.com/@kurzgesagt" target="_blank">Kurzgesagt - In a Nutshell</a>,
+  <a href="https://www.youtube.com/@harvardbusinessreview" target="_blank">Harvard Business Review</a> and <a href="https://www.youtube.com/@marktilbury" target="_blank">Mark Tilbury (sometimes)</a>! `,
+
+  csDegree: `This is my Computer Science degree. I majored in Software Development and I am keen on venturing into frontend or backend development. 
+  I hung it on the wall because I'm proud of it. It was a very pratical degree so I think it gave me a good foundation.`,
+
+  bed: `This where I sleep. Sometimes I lie awake, fascinated by the possibilities of this world. Then, I remind 
+  myself, that it is within my power to reach the skies.`,
+  
+  //todo
+  letter: `This is my desk and on it is my letter of commendation. <a href="" target="_blank">Check it out?</a>
+  Contact me at jerroldwongyz@gmail.com if you have any interesting job opportunities!`,
+  
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
+  If you want to learn more about my other projects: <a href="https://github.com/Jerroldwyz" target="_blank">Check out my GitHub</a>`,
+  
+  library: `There are a lot of books on my shelves. I like reading non-fictional books to learn a variety of topics. I mainly read books
+  for personal development, inspiration, and intellectual stimulation. I have a curious mind.`,
+  
+  exit: `If you want to exit Jerrold's portfolio, just close the tab.`,
+};
