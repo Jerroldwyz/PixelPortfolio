c2b9407713e868256ced9611ddfea8b803870122
Jerrwyz <jerroldwongyz@gmail.com>
2024-04-08 14:10:57 +1000

    implemented sprite walking direction

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31, 
   anims: {
-    "idle-down": 936,
-    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-    "idle-side": 975,
-    "walk-side": { from:975, to: 978, loop: true, speed: 8},
-    "idle-up": 1014,
-    "walk-up": { from: 1014, to:1017, loop: true, speed: 8}
+    "idle-down": 940,
+    "walk-down": { from: 940, to: 943, loop: true, speed: 8 },
+    "idle-side": 979,
+    "walk-side": { from:979, to: 982, loop: true, speed: 8},
+    "idle-up": 1018,
+    "walk-up": { from: 1018, to:1021, loop: true, speed: 8}
   },
 })
 
@@ -97,6 +97,45 @@ k.scene("main", async() => {
 
     const worldMousePos = k.toWorld(k.mousePos());
     player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound && 
+      mouseAngle < upperBound &&
+      player.curAnim() !== "walk-up"
+    ){
+      player.play("walk-up");
+      player.direction = "up";
+      return;
+    }
+
+    if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim() !== "walk-down"
+    ){
+      player.play("walk-down");
+      player.direction = "down";
+      return;
+    }
+
+    if(Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side")
+      player.direction = "right";
+      return;
+    }
+
+    if(Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side")
+      player.direction = "left";
+      return;
+    }
   });
 });
 
